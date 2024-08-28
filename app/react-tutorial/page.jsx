8642c0f8ee5c6155b4a743946fc50d8cde452866
Khanchit202 <khanchit202@gmail.com>
2024-08-28 14:18:53 +0700

    แสดงข้อความจาก input

--- app/react-tutorial/page.jsx
+++ app/react-tutorial/page.jsx
@@ -1,41 +1,25 @@
-import React from 'react'
+"use client"
+import React, {useState} from 'react'
 
 function MyButton() {
     return (<button>Click me</button>)
   }
 
 export default function Page() {
-    const isLoggedIn = true;
-    let content;
-    const product = [
-        {title: 'Product 1', desc: 'My dasc 1'},
-        {title: 'Product 2', desc: 'My dasc 2'},
-        {title: 'Product 3', desc: 'My dasc 3'},
-        {title: 'Product 4', desc: 'My dasc 4'}
-    ]
 
-    if (isLoggedIn){
-        content = <h1>You're logged in 1</h1>
-    }else(
-        content = <h1>Please login</h1>
-    ) 
+    const [username, setUser] = useState({username:""})
 
-  return (
+    function handleClick(event) {
+        let usernameValue = event.target.value;
+        setUser({...username, username: usernameValue})
+    }
+
+    return (
 
     <div>
-        {content}
-
-        {isLoggedIn ? <h1>You're logged in 2</h1> : <h1>Please login 2</h1>}
-
-        {isLoggedIn && <h1>You're logged in 3</h1>}
-
-        <ul>
-            {product.map((item, index) => (
-                <li key={index}>Title:{item.title} Desh: {item.desc}</li>
-            ))}
-        </ul>
-
+        <h1>Username : {username.username}</h1>
+        <input type="text" placeholder='username' value={username.username} onChange={handleClick}/>
     </div>
 
-  );
+    );
 }
